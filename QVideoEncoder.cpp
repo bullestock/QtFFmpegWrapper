3303103d01650458b1362c512171072431ffa4a9
Torsten Martinsen <tma@gatehouse.dk>
2017-12-20 14:06:05 +0100

    Add logging.

--- QVideoEncoder.cpp
+++ QVideoEncoder.cpp
@@ -128,6 +128,7 @@ bool QVideoEncoder::createFile(QString fileName, unsigned width, unsigned height
    if (pCodecCtx->codec_id == ffmpeg::AV_CODEC_ID_H264)
    {
       // Set H.264 defaults
+      av_log(NULL, AV_LOG_INFO, "Setting H.264 defaults");
       pCodecCtx->me_range = 16;
       pCodecCtx->max_qdiff = 4;
       pCodecCtx->qmin = 10;
